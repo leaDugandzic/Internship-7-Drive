604d71d6eeaa089fab1080ddb9c57af70bb965c6
leaDugandzic <lea.dugandzic07@gmail.com>
2025-01-02 19:44:35 +0100

    Adding CommentService and CommentRepository

---
+++ DUMPDrive.Domain/Repositories/CommentRepository.cs
@@ -0,0 +1,23 @@
+﻿using DUMPDrive.Domain.Entities;
+
+namespace DUMPDrive.Data.Repositories;
+
+public class CommentRepository
+{
+    private readonly List<Comment> _comments;
+
+    public CommentRepository(List<Comment> comments)
+    {
+        _comments = comments;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public List<Comment> GetCommentsByFileId(int fileId)
+    {
+        return _comments.Where(c => c.FileItemId == fileId).ToList();
+    }
+}
---
+++ DUMPDrive.Domain/Services/CommentService.cs
@@ -0,0 +1,33 @@
+﻿using DUMPDrive.Domain.Entities;
+using DUMPDrive.Data.Repositories;
+
+namespace DUMPDrive.Domain.Services;
+
+public class CommentService
+{
+    private readonly CommentRepository _commentRepository;
+
+    public CommentService(CommentRepository commentRepository)
+    {
+        _commentRepository = commentRepository;
+    }
+
+    public void AddComment(string email, string content, int fileId)
+    {
+        var comment = new Comment
+        {
+            Email = email,
+            Content = content,
+            FileItemId = fileId,
+            Date = DateTime.Now
+        };
+
+        _commentRepository.AddComment(comment);
+    }
+
+    public List<Comment> GetCommentsForFile(int fileId)
+    {
+        return _commentRepository.GetCommentsByFileId(fileId);
+    }
+}
+
