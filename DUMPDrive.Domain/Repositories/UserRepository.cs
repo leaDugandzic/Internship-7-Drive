e1dff7412fe61635c92a01726b8e74d4d34d413d
leaDugandzic <lea.dugandzic07@gmail.com>
2025-01-02 17:40:20 +0100

    Added UserRepository and folder Repositories

---
+++ DUMPDrive.Domain/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using DUMPDrive.Domain.Entities;
+
+public class UserRepository
+{
+    private readonly List<User> _users;
+
+    public UserRepository(List<User> users)
+    {
+        _users = users;
+    }
+
+    public bool AddUser(User user)
+    {
+        if (_users.Any(u => u.Email == user.Email))
+            return false;
+
+        user.Id = _users.Count + 1;
+        _users.Add(user);
+        return true;
+    }
+
+    public User? GetUser(string email, string passwordHash)
+    {
+        return _users.SingleOrDefault(u => u.Email == email && u.PasswordHash == passwordHash);
+    }
+}
